a8d18c08edd853947fe68580ae37a26bd0f22e37
Maksim Korzhov <isitbux@gmail.com>
2017-09-25 22:10:00 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+
+const scss = require("../style/style.scss");
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+);
